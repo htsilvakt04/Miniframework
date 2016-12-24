7a09d3f61156208bdeea6051ec1ba1ecb8c68f9f
Trung-Silva <htsilvakt04@gmail.com>
2016-12-24 13:58:09 +0700

    Finish create Container class

--- index.php
+++ index.php
@@ -1,3 +1,24 @@
 <?php
+require("vendor/autoload.php");
+$app = new App\App;
 
-echo "dawd";
+$container = $app->getContainer();
+
+$container["config"] = function () {
+  return [
+    "db_driver" => "mysql",
+    "db_host" => "localhost",
+    "db_name" => "test",
+    "db_user" => "root",
+    "db_password" => ""
+  ];
+};
+
+$container["db"] = function ($c) {
+  return new PDO(
+    "{$c->config['db_driver']}:host={$c->config['db_host']};dbname={$c->config['db_name']}",
+    $c->config['db_user'],
+    $c->config['db_password']
+  );
+};
+var_dump($container->db);
